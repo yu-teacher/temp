4ec96b0b756dbfcb1ba2a0e8fde630dc7553165f
yu-teacher <yusm1231@gmail.com>
2023-01-19 23:01:18 +0900

    최초커밋

---
+++ src/main/java/com/my/sb/TempSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.my.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempSpringBootApplication.class, args);
+	}
+
+}
