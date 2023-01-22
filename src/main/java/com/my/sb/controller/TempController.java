8776ebecf18c4a3dd6d40330278fe6fa96e837f0
yu-teacher <yusm1231@gmail.com>
2023-01-23 00:08:25 +0900

    연습 음....

--- src/main/java/com/my/sb/controller/TempController.java
+++ src/main/java/com/my/sb/controller/TempController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/qqq/*")
+@RequestMapping("/sample/*")
 public class TempController {
 
-    @GetMapping("/www")
-    public String www(){
+    @GetMapping("/helloworld")
+    public void www(){
 
-        return "/www";
     }
 }
